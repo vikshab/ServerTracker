756a9c2d286d385c86b73aecc524551cd53b05a3
Victoria <vikshab2010@hotmail.com>
2016-09-01 13:08:35 -0700

    Added routes for record and average

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var servers = require('../controllers/servers');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* PUT new record into database (in-memory). */
+router.put('/record', function(req, res, next) {
+  servers.serversController.record(req, res);
+});
+
+/* GET average for the specific server. */
+router.get('/average/:server_name', function(req, res, next) {
+  return servers.serversController.average(req, res);
+});
+
 
 module.exports = router;
